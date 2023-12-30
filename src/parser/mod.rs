f263e7c435bbc77a302f1e7825e294e3298d4441
HusseinElguindi <34726859+HusseinElguindi@users.noreply.github.com>
2023-12-30 11:14:54 -0500

    terminating token type guard readability improvement

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -41,18 +41,19 @@ impl Parser {
         // todo!("return a single document token")
     }
 
-    // Parses and advances the token iterable the optionally specified token type is specified; if encountered, that token is also consumned.
+    /// Parses and advances the token iterator until the optionally specified token type
+    /// encountered; if encountered, the terminating token type is also consumned. If a terminating
+    /// token is not specified, parsing continues until the iterator is empty.
     fn _parse(&mut self, until_tpe: Option<TokenType>) -> Vec<Box<dyn Node>> {
         let mut nodes = vec![]; // this is basically the AST
 
         while let Some(token) = self.tokens.peek() {
             // Break if we match a token with the passed `until_tpe` type
-            if until_tpe
-                .as_ref()
-                .is_some_and(|until_tpe| *until_tpe == token.tpe)
-            {
-                self.tokens.next();
-                break;
+            if let Some(ref until_tpe) = until_tpe {
+                if *until_tpe == token.tpe {
+                    self.tokens.next();
+                    break;
+                }
             }
 
             // Each node's parser is expected to advance the token iterator
